e3de4e35f241148f9d734a91742842293c5cf71d
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-24 12:47:36 -0400

    Create base Decorator class

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
-require './nameable.rb'
+require './nameable'
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100_000)
     @name = name
     @age = age
