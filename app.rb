899bc5c01764fe9681ca472350e18873243ed8cb
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-26 13:02:30 -0400

    Create methods create_person and create_student

--- app.rb
+++ app.rb
@@ -27,6 +27,44 @@ class App
     end
   end
 
+  def create_person
+    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    person_number = gets.chomp.to_i
+    case person_number
+    when 1
+        create_student
+    when 2
+        create_teacher
+    else
+        puts "Not valid number"
+    end
+  end
+
+  def create_student
+    print "Age: "
+    age = gets.chomp.to_i
+    print "Name: "
+    name = gets.chomp
+    print "Classroom: "
+    classroom = gets.chomp.capitalize
+    print "Has parent permission? [Y/N]: "
+    parent_permission = gets.chomp.upcase
+    if parent_permission === "Y"
+        parent_permission = true
+    elsif parent_permission === "N"
+        parent_permission = false
+    else
+        return puts "Not valid permission answer"
+    end
+    newStudent = Student.new(classroom, age, name, parent_permission)
+    @people << newStudent
+    puts "Person created successfully"
+  end
+
+  def create_teacher
+    puts "creating a teacher"
+  end
+
   def create_book
     print 'Title: '
     title = gets.chomp.capitalize
@@ -40,4 +78,4 @@ class App
 end
 
 newapp = App.new
-puts newapp.list_all_people
+puts newapp.create_person
