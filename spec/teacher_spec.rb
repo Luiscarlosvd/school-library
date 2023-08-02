c165e2dea3d796df2f1dd28867faec9a7e2761bc
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-02 13:31:36 -0400

    Add tests for classroom class inside classroom_spec.rb

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../teacher'
 
 describe Teacher do
-  context "When using teacher class method" do
+  context 'When using teacher class method' do
     it 'Has to return true when using can_use_services?' do
-      teacher = Teacher.new('Mathematics',30, 'Luis')
+      teacher = Teacher.new('Mathematics', 30, 'Luis')
 
       result = teacher.can_use_services?
       expect(result).to be true
