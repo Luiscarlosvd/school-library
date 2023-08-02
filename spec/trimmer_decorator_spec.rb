785e307a05b7cb042d2ad6239575c4758b5b9319
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-02 14:10:58 -0400

    Add tests for trimmerDecorator class and Nameable class

---
+++ spec/trimmer_decorator_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../trimmer_decorator'
+
+describe TrimmerDecorator do
+  context "When using TrimmerDecorator method" do
+    let(:nameable_mock) { double('nameable_mock', correct_name: 'Luis Carlos') }
+    let(:decorator) { TrimmerDecorator.new(nameable_mock) }
+
+    it 'cuts the name if the length of the name is bigger than 10' do
+      expect(decorator.correct_name).to eq('Luis Carlo')
+    end
+  end
+end
