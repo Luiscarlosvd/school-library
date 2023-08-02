4a90b1537daa80052b6836448eb476300590f409
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-02 13:05:49 -0400

    Add tests for Student class and his methods inside student_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,23 +1,23 @@
-require_relative '../book.rb'
-require_relative '../rental.rb'
+require_relative '../book'
+require_relative '../rental'
 
 describe Book do
-    context 'When testing the book class' do
-        it "Has to create an instance of book class" do
-            book = Book.new("Luis", "Adeyemi")
+  context 'When testing the book class' do
+    it 'Has to create an instance of book class' do
+      book = Book.new('Luis', 'Adeyemi')
 
-            expect(book).to be_instance_of Book
-        end
-
-        it "Has to create a rental when using add_rental method" do
-            book = Book.new("Luis", "Adeyemi")
-            person = double("person")
-            allow(person).to receive(:name) {"Luis Carlos"}
-            allow(person).to receive(:rentals) {[]}
-
-            result = book.add_rental(person, "00/00/00")
-
-            expect(result).to be_instance_of Rental
-        end
+      expect(book).to be_instance_of Book
     end
+
+    it 'Has to create a rental when using add_rental method' do
+      book = Book.new('Luis', 'Adeyemi')
+      person = double('person')
+      allow(person).to receive(:name) { 'Luis Carlos' }
+      allow(person).to receive(:rentals) { [] }
+
+      result = book.add_rental(person, '00/00/00')
+
+      expect(result).to be_instance_of Rental
+    end
+  end
 end
